a02847eda0506ae14134fc0c19fa2f439c31c0a8
Am-Coder <39997970+Am-Coder@users.noreply.github.com>
2019-01-31 01:01:03 +0530

    Made using spring websocket

---
+++ WebChat/src/main/java/com/chat/demo/controller/ChatController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chat.demo.controller;
+
+import com.chat.demo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author HP
+ */
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, 
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    
+    @MessageMapping("/error")
+    public void error(){
+        System.out.println("Hell");
+    }
+}
